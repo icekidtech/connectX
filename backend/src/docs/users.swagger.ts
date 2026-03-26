fe715fe552ab6b68113b238422a19cb9923cc662
icekidtech <edwinidopise@gmail.com>
2026-03-26 01:35:10 +0100

    Add Swagger documentation for users module endpoints

---
+++ backend/src/docs/users.swagger.ts
@@ -0,0 +1,21 @@
+/**
+ * Users Module Swagger Documentation
+ * 
+ * This file contains all Swagger decorators and schemas for user endpoints.
+ * Endpoints will be added as Phase 2 is implemented.
+ * 
+ * Planned Endpoints (Phase 2):
+ * - GET /users/:id
+ * - PUT /users/:id
+ * - GET /users/:id/preferences
+ * - PUT /users/:id/preferences
+ * - GET /interests
+ * - POST /users/:id/interests
+ * - DELETE /users/:id/interests/:interestId
+ * - GET /users/:id/photos
+ * - POST /users/:id/photos
+ * - DELETE /users/:id/photos/:photoId
+ * - GET /users/:id/verification
+ */
+
+export const usersSwaggerFile = 'src/docs/users.swagger.ts';
