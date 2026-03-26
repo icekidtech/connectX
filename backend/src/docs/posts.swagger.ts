66b8e47a0c11ac33b2e703d66b6aa4311320ce70
icekidtech <edwinidopise@gmail.com>
2026-03-26 01:37:43 +0100

    Add Swagger documentation for posts module endpoints

---
+++ backend/src/docs/posts.swagger.ts
@@ -0,0 +1,23 @@
+/**
+ * Posts Module Swagger Documentation
+ * 
+ * This file contains all Swagger decorators and schemas for post endpoints.
+ * Endpoints will be added as Phase 3 is implemented.
+ * 
+ * Planned Endpoints (Phase 3):
+ * - GET /posts/feed
+ * - POST /posts
+ * - GET /posts/:id
+ * - DELETE /posts/:id
+ * - PATCH /posts/:id
+ * - POST /posts/:id/like
+ * - GET /posts/:id/likes
+ * - POST /posts/:id/comments
+ * - GET /posts/:id/comments
+ * - DELETE /comments/:commentId
+ * - PUT /comments/:commentId
+ * - POST /posts/:id/media
+ * - DELETE /posts/:id/media/:mediaId
+ */
+
+export const postsSwaggerFile = 'src/docs/posts.swagger.ts';
