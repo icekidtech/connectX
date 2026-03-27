db3369d847b4ff1abfb76b391fe72aeb69cb85da
icekidtech <edwinidopise@gmail.com>
2026-03-27 16:10:55 +0100

    Refactor import statement for cookie-parser to use default import syntax

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import { setupSwagger } from './config/swagger';
 import { HttpExceptionFilter, AllExceptionsFilter } from './common/filters/http-exception.filter';
